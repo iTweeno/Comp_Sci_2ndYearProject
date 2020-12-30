e0ee83ef08a161e933611e3f8289621e495477f1
David <itweeno@gmail.com>
2020-12-30 16:41:58 +0000

    Added another menu

--- main.c
+++ main.c
@@ -1,40 +1,43 @@
 #include<stdio.h>
 #include"main.h"
 #include"ex2_3.h"
+#include"ex6_7.h"
+
 
 EMPRESA empresa = { 0,NULL,NULL };
 
 int main() {
-    int op;
-    do {
-        printf("\t\tGestor para a Empresa\n\n");
-        printf("Escolha a opcao:\n");
-        printf("\t1 - Gestao Edificios\n");
-        printf("\t2\n");
-        printf("\t3\n");
-        printf("\t4\n");
-        printf("\t0-Sair\n");
-        scanf("%d", &op);
-        switch (op) {
-        case 1:
-            Gestao_Edificios();
-            break;
-        case 2:
+	int op;
+	do {
+		printf("\t\tGestor para a Empresa\n\n");
+		printf("Escolha a opcao:\n");
+		printf("\t1-Gestao Edificios\n");
+		printf("\t2-Relatorios\n");
+		printf("\t3\n");
+		printf("\t4\n");
+		printf("\t0-Sair\n");
+		scanf("%d", &op);
+		switch (op) {
+		case 1:
+			Gestao_Edificios();
+			break;
+		case 2:
+			Relatorios();
+			break;
+		case 3:
 
-            break;
-        case 3:
+			break;
+		case 4:
 
-            break;
-        case 4:
+			break;
+		case 0:
 
-            break;
-        case 0:
-            break;
-        default:
+			return 0;
+		default:
 
-            printf("Opcao invalida!\n\n");
-        }
-    } while (op != 0);
+			printf("Opcao invalida!\n\n");
+		}
+	} while (op != 0);
 
-    return 0;
+	return 0;
 };
