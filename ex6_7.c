329d529b0466acdbed0cbb7bacf5fea14b4e0367
David <itweeno@gmail.com>
2020-12-30 16:39:00 +0000

    Should be done, needs testing

--- ex6_7.c
+++ ex6_7.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 #include"ex2_3.h"
 #include"main.h"
-//Billing and sort missing
+//sort missing
+
+typedef struct Taxa {
+	int moneyCount;
+	int edificio;
+	int estudio;
+	int estadiaCount;
+} TAXA;
 int Relatorios() {
 	int op;
 	do {
@@ -20,19 +27,19 @@ int Relatorios() {
 			Taxa_Ocupacao_Estudio(&empresa, 1, 1, 1, 1, 2010, 1, 1, 2030, "print");
 			break;
 		case 2:
-			Taxa_Ocupacao_Edificio(&empresa, 1, 1, 1, 2010, 1, 1, 2030, "print");
+			Taxa_Ocupacao_Edificio(&empresa, 1, 1, 1, 2010, 1, 1, 2030, "print", "crescente");
 			break;
 		case 3:
-			Taxa_Ocupacao_Lote(&empresa, 1, 1, 2010, 1, 1, 2030, "print");
+			Taxa_Ocupacao_Lote(&empresa, 1, 1, 2010, 1, 1, 2030, "print", "crescente");
 			break;
 		case 4:
 			Taxa_Faturacao_Estudio(&empresa, 1, 1, 1, 1, 2010, 1, 1, 2030, "print");
 			break;
 		case 5:
-			Taxa_Faturacao_Edificio(&empresa, 1, 1, 1, 2010, 1, 1, 2030, "print");
+			Taxa_Faturacao_Edificio(&empresa, 1, 1, 1, 2010, 1, 1, 2030, "print", "crescente");
 			break;
 		case 6:
-			Taxa_Faturacao_Lote(&empresa, 1, 1, 2010, 1, 1, 2030, "print");
+			Taxa_Faturacao_Lote(&empresa, 1, 1, 2010, 1, 1, 2030, "print", "crescente");
 			break;
 		case 0:
 			break;
@@ -45,6 +52,7 @@ int Relatorios() {
 	return 0;
 };
 
+
 void swap(int* xp, int* yp)
 {
 	int temp = *xp;
@@ -52,18 +60,54 @@ void swap(int* xp, int* yp)
 	*yp = temp;
 }
 
-void selectionSort(AGENDA arr[], int n)
+void selectionSortCrescente(TAXA arr[], int n, char type[])
 {
 	int i, j, min_idx;
 
 	for (i = 0; i < n - 1; i++) {
 
 		min_idx = i;
-		for (j = i + 1; j < n; j++)
-			if (arr[j].id < arr[min_idx].id)
-				min_idx = j;
+		for (j = i + 1; j < n; j++) {
+			if (strcmp("money", type) == 0) {
+				if (arr[j].moneyCount < arr[min_idx].moneyCount) {
 
+					min_idx = j;
+				}
+			}
+			else {
+				if (arr[j].estadiaCount < arr[min_idx].estadiaCount) {
 
+					min_idx = j;
+				}
+			}
+
+		}
+		swap(&arr[min_idx], &arr[i]);
+	}
+}
+
+void selectionSortDecrescente(TAXA arr[], int n, int type[])
+{
+	int i, j, min_idx;
+
+	for (i = 0; i < n - 1; i++) {
+
+		min_idx = i;
+		for (j = i + 1; j < n; j++) {
+			if (strcmp("money", type) == 0) {
+				if (arr[j].moneyCount > arr[min_idx].moneyCount) {
+
+					min_idx = j;
+				}
+			}
+			else {
+				if (arr[j].estadiaCount > arr[min_idx].estadiaCount) {
+
+					min_idx = j;
+				}
+			}
+
+		}
 		swap(&arr[min_idx], &arr[i]);
 	}
 }
@@ -122,7 +166,7 @@ int Taxa_Ocupacao_Estudio(EMPRESA* empresa, int idEdificio, int idEstudio, int d
 			fclose(fp);
 		}
 		return;
-	}
+	}             
 	if (pcurrent->num_estudios == 0) {
 		return printf("Nao existem estudios");
 	}
@@ -160,10 +204,12 @@ int Taxa_Ocupacao_Estudio(EMPRESA* empresa, int idEdificio, int idEstudio, int d
 	}
 }
 
-int Taxa_Ocupacao_Edificio(EMPRESA* empresa, int idEdificio, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[]) {
+int Taxa_Ocupacao_Edificio(EMPRESA* empresa, int idEdificio, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[], char order[]) {
 	DATA inicio = { data_inicio_dia,data_inicio_mes ,data_inicio_ano };
 	DATA fim = { data_fim_dia,data_fim_mes ,data_fim_ano };
-	int estadiaCount = 0;
+	int estudioCount = 0;
+	TAXA* taxa;
+	taxa = (TAXA*)malloc(sizeof(TAXA) * 20);
 	if (empresa->edificio == NULL) {
 		printf("Nao ha edificios\n");
 		return;
@@ -181,75 +227,92 @@ int Taxa_Ocupacao_Edificio(EMPRESA* empresa, int idEdificio, int data_inicio_dia
 		if (empresa->edificio->num_estudios == 0) {
 			return printf("Nao existem estudios");
 		}
-
 		for (int index = 0; index < empresa->edificio->num_estudios; index++) {
+			taxa[estudioCount].estudio = empresa->edificio->estudios[index].id;
+			taxa[estudioCount].estadiaCount = 0;
 			AGENDA* pprevcl = NULL, * pcurrentcl = empresa->edificio->estudios[index].agenda;
 			while (pcurrentcl != NULL) {
 				if (strcmp(pcurrentcl->evento, "estadia") != 0) {
 					continue;
 				}
+
 				if (checkIfDatesCoincide(inicio, pcurrentcl->data_inicio) && checkIfDatesCoincide(pcurrentcl->data_inicio, fim)) {
-					estadiaCount++;
+					taxa[estudioCount].estadiaCount++;
 				}
 				pprevcl = pcurrentcl;
 				pcurrentcl = pcurrentcl->next;
 			}
+			estudioCount++;
+		}
+		for (int i = 0; i < estudioCount; i++) {
 			if (strcmp(type, "print") == 0) {
-				printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, estadiaCount, empresa->edificio->estudios[index].id);
+				printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].estadiaCount, taxa[i].estudio);
 			}
 			else if (strcmp(type, "file") == 0) {
 				FILE* fp = fopen("taxa_ocupacao.txt", "w");
 				if (fp == NULL) {
 					printf("Erro ao abrir ficheiro");
 				}
-				fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, estadiaCount, empresa->edificio->estudios[index].id);
+				fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].estadiaCount, taxa[i].estudio);
 				fclose(fp);
 			}
 			else {
 				printf("Opcao invalida");
 			}
-			estadiaCount = 0;
 		}
 		return;
 	}
-	if (empresa->edificio->num_estudios == 0) {
+	if (pcurrent->num_estudios == 0) {
 		return printf("Nao existem estudios");
 	}
 
 	for (int index = 0; index < pcurrent->num_estudios; index++) {
+		taxa[estudioCount].estudio = pcurrent->estudios[index].id;
+		taxa[estudioCount].estadiaCount = 0;
 		AGENDA* pprevcl = NULL, * pcurrentcl = pcurrent->estudios[index].agenda;
 		while (pcurrentcl != NULL) {
 			if (strcmp(pcurrentcl->evento, "estadia") != 0) {
 				continue;
 			}
+
 			if (checkIfDatesCoincide(inicio, pcurrentcl->data_inicio) && checkIfDatesCoincide(pcurrentcl->data_inicio, fim)) {
-				estadiaCount++;
+				taxa[estudioCount].estadiaCount++;
 			}
 			pprevcl = pcurrentcl;
 			pcurrentcl = pcurrentcl->next;
 		}
+		estudioCount++;
+	}
+	if (strcmp("decrescente", order) == 0) {
+		selectionSortDecrescente(taxa, estudioCount, "estadia");
+	}
+	else {
+		selectionSortCrescente(taxa, estudioCount, "estadia");
+	}
+	for (int i = 0; i < estudioCount; i++) {
 		if (strcmp(type, "print") == 0) {
-			printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, estadiaCount, pcurrent->estudios[index].id);
+			printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].estadiaCount, taxa[i].estudio);
 		}
 		else if (strcmp(type, "file") == 0) {
 			FILE* fp = fopen("taxa_ocupacao.txt", "w");
 			if (fp == NULL) {
 				printf("Erro ao abrir ficheiro");
 			}
-			fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, estadiaCount, pcurrent->estudios[index].id);
+			fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].estadiaCount, taxa[i].estudio);
 			fclose(fp);
 		}
 		else {
 			printf("Opcao invalida");
 		}
-		estadiaCount = 0;
 	}
 }
 
-int Taxa_Ocupacao_Lote(EMPRESA* empresa, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[]) {
+int Taxa_Ocupacao_Lote(EMPRESA* empresa, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[], char order[]) {
 	DATA inicio = { data_inicio_dia,data_inicio_mes ,data_inicio_ano };
 	DATA fim = { data_fim_dia,data_fim_mes ,data_fim_ano };
-	int estadiaCount = 0;
+	int estudioCount = 0;
+	TAXA* taxa;
+	taxa = (TAXA*)malloc(sizeof(TAXA) * 20);
 	if (empresa->edificio == NULL) {
 		printf("Nao ha edificios\n");
 		return;
@@ -261,35 +324,47 @@ int Taxa_Ocupacao_Lote(EMPRESA* empresa, int data_inicio_dia, int data_inicio_me
 		}
 
 		for (int index = 0; index < pcurrent->num_estudios; index++) {
-			AGENDA* pprevcl = NULL, * pcurrentcl = empresa->edificio->estudios[index].agenda;
+			taxa[estudioCount].edificio = pcurrent->id;
+			taxa[estudioCount].estudio = pcurrent->estudios[index].id;
+			taxa[estudioCount].estadiaCount = 0;
+			AGENDA* pprevcl = NULL, * pcurrentcl = pcurrent->estudios[index].agenda;
 			while (pcurrentcl != NULL) {
 				if (strcmp(pcurrentcl->evento, "estadia") != 0) {
 					continue;
 				}
+
 				if (checkIfDatesCoincide(inicio, pcurrentcl->data_inicio) && checkIfDatesCoincide(pcurrentcl->data_inicio, fim)) {
-					estadiaCount++;
+					taxa[estudioCount].estadiaCount++;
 				}
 				pprevcl = pcurrentcl;
 				pcurrentcl = pcurrentcl->next;
 			}
-			if (strcmp(type, "print") == 0) {
-				printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d no edificio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, estadiaCount, pcurrent->estudios[index].id, pcurrent->id);
-			}
-			else if (strcmp(type, "file") == 0) {
-				FILE* fp = fopen("taxa_ocupacao.txt", "w");
-				if (fp == NULL) {
-					printf("Erro ao abrir ficheiro");
-				}
-				fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d no edificio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, estadiaCount, pcurrent->estudios[index].id, pcurrent->id);
-				fclose(fp);
-			}
-			else {
-				printf("Opcao invalida");
-			}
+			estudioCount++;
 		}
-		estadiaCount = 0;
 		pprev = pcurrent;
 		pcurrent = pcurrent->next;
+	}
+	if (strcmp("decrescente", order) == 0) {
+		selectionSortDecrescente(taxa, estudioCount, "estadia");
+	}
+	else {
+		selectionSortCrescente(taxa, estudioCount, "estadia");
+	}
+	for (int i = 0; i < estudioCount; i++) {
+		if (strcmp(type, "print") == 0) {
+			printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].estadiaCount, taxa[i].estudio);
+		}
+		else if (strcmp(type, "file") == 0) {
+			FILE* fp = fopen("taxa_ocupacao.txt", "w");
+			if (fp == NULL) {
+				printf("Erro ao abrir ficheiro");
+			}
+			fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].estadiaCount, taxa[i].estudio);
+			fclose(fp);
+		}
+		else {
+			printf("Opcao invalida");
+		}
 	}
 
 }
@@ -380,10 +455,12 @@ int Taxa_Faturacao_Estudio(EMPRESA* empresa, int idEdificio, int idEstudio, int 
 	}
 }
 
-int Taxa_Faturacao_Edificio(EMPRESA* empresa, int idEdificio, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[]) {
+int Taxa_Faturacao_Edificio(EMPRESA* empresa, int idEdificio, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[], char order[]) {
 	DATA inicio = { data_inicio_dia,data_inicio_mes ,data_inicio_ano };
 	DATA fim = { data_fim_dia,data_fim_mes ,data_fim_ano };
-	int moneyAmount = 0;
+	int estudioCount = 0;
+	TAXA* taxa;
+	taxa = (TAXA*)malloc(sizeof(TAXA) * 20);
 	if (empresa->edificio == NULL) {
 		printf("Nao ha edificios\n");
 		return;
@@ -403,73 +480,98 @@ int Taxa_Faturacao_Edificio(EMPRESA* empresa, int idEdificio, int data_inicio_di
 		}
 
 		for (int index = 0; index < empresa->edificio->num_estudios; index++) {
+			taxa[estudioCount].estudio = empresa->edificio->estudios[index].id;
+			taxa[estudioCount].moneyCount = 0;
 			AGENDA* pprevcl = NULL, * pcurrentcl = empresa->edificio->estudios[index].agenda;
 			while (pcurrentcl != NULL) {
 				if (strcmp(pcurrentcl->evento, "estadia") != 0) {
 					continue;
 				}
+
 				if (checkIfDatesCoincide(inicio, pcurrentcl->data_inicio) && checkIfDatesCoincide(pcurrentcl->data_inicio, fim)) {
-					moneyAmount += pcurrentcl->preco;
+					taxa[estudioCount].moneyCount+=pcurrentcl->preco;
 				}
 				pprevcl = pcurrentcl;
 				pcurrentcl = pcurrentcl->next;
 			}
+			estudioCount++;
+		}
+		if (strcmp("decrescente", order) == 0) {
+			selectionSortDecrescente(taxa, estudioCount, "money");
+		}
+		else {
+			selectionSortCrescente(taxa, estudioCount,"money");
+		}
+		for (int i = 0; i < estudioCount; i++) {
 			if (strcmp(type, "print") == 0) {
-				printf("Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, moneyAmount, empresa->edificio->estudios[index].id);
+				printf("Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].moneyCount, taxa[i].estudio);
 			}
 			else if (strcmp(type, "file") == 0) {
 				FILE* fp = fopen("taxa_ocupacao.txt", "w");
 				if (fp == NULL) {
 					printf("Erro ao abrir ficheiro");
 				}
-				fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, moneyAmount, empresa->edificio->estudios[index].id);
+				fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].moneyCount, taxa[i].estudio);
 				fclose(fp);
 			}
 			else {
 				printf("Opcao invalida");
 			}
-			moneyAmount = 0;
 		}
 		return;
 	}
-	if (empresa->edificio->num_estudios == 0) {
+
+	if (pcurrent->num_estudios == 0) {
 		return printf("Nao existem estudios");
 	}
 
 	for (int index = 0; index < pcurrent->num_estudios; index++) {
-		AGENDA* pprevcl = NULL, * pcurrentcl = empresa->edificio->estudios[index].agenda;
+		taxa[estudioCount].estudio = pcurrent->estudios[index].id;
+		taxa[estudioCount].moneyCount = 0;
+		AGENDA* pprevcl = NULL, * pcurrentcl = pcurrent->estudios[index].agenda;
 		while (pcurrentcl != NULL) {
 			if (strcmp(pcurrentcl->evento, "estadia") != 0) {
 				continue;
 			}
+
 			if (checkIfDatesCoincide(inicio, pcurrentcl->data_inicio) && checkIfDatesCoincide(pcurrentcl->data_inicio, fim)) {
-				moneyAmount += pcurrentcl->preco;
+				taxa[estudioCount].moneyCount+=pcurrentcl->preco;
 			}
 			pprevcl = pcurrentcl;
 			pcurrentcl = pcurrentcl->next;
 		}
+		estudioCount++;
+	}
+	if (strcmp("decrescente", order) == 0) {
+		selectionSortDecrescente(taxa, estudioCount,"money");
+	}
+	else {
+		selectionSortCrescente(taxa, estudioCount,"money");
+	}
+	for (int i = 0; i < estudioCount; i++) {
 		if (strcmp(type, "print") == 0) {
-			printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, moneyAmount, pcurrent->estudios[index].id);
+			printf("Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].moneyCount, taxa[i].estudio);
 		}
 		else if (strcmp(type, "file") == 0) {
 			FILE* fp = fopen("taxa_ocupacao.txt", "w");
 			if (fp == NULL) {
 				printf("Erro ao abrir ficheiro");
 			}
-			fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, moneyAmount, pcurrent->estudios[index].id);
+			fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].moneyCount, taxa[i].estudio);
 			fclose(fp);
 		}
 		else {
 			printf("Opcao invalida");
 		}
-		moneyAmount = 0;
 	}
 }
 
-int Taxa_Faturacao_Lote(EMPRESA* empresa, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[]) {
+int Taxa_Faturacao_Lote(EMPRESA* empresa, int data_inicio_dia, int data_inicio_mes, int data_inicio_ano, int data_fim_dia, int data_fim_mes, int data_fim_ano, char type[], char order[]) {
 	DATA inicio = { data_inicio_dia,data_inicio_mes ,data_inicio_ano };
 	DATA fim = { data_fim_dia,data_fim_mes ,data_fim_ano };
-	int moneyCount = 0;
+	int estudioCount = 0;
+	TAXA* taxa;
+	taxa = (TAXA*)malloc(sizeof(TAXA) * 20);
 	if (empresa->edificio == NULL) {
 		printf("Nao ha edificios\n");
 		return;
@@ -481,35 +583,47 @@ int Taxa_Faturacao_Lote(EMPRESA* empresa, int data_inicio_dia, int data_inicio_m
 		}
 
 		for (int index = 0; index < pcurrent->num_estudios; index++) {
-			AGENDA* pprevcl = NULL, * pcurrentcl = empresa->edificio->estudios[index].agenda;
+			taxa[estudioCount].edificio = pcurrent->id;
+			taxa[estudioCount].estudio = pcurrent->estudios[index].id;
+			taxa[estudioCount].moneyCount = 0;
+			AGENDA* pprevcl = NULL, * pcurrentcl = pcurrent->estudios[index].agenda;
 			while (pcurrentcl != NULL) {
 				if (strcmp(pcurrentcl->evento, "estadia") != 0) {
 					continue;
 				}
+
 				if (checkIfDatesCoincide(inicio, pcurrentcl->data_inicio) && checkIfDatesCoincide(pcurrentcl->data_inicio, fim)) {
-					moneyCount += pcurrentcl->preco;
+					taxa[estudioCount].moneyCount+=pcurrentcl->preco;
 				}
 				pprevcl = pcurrentcl;
 				pcurrentcl = pcurrentcl->next;
 			}
-			if (strcmp(type, "print") == 0) {
-				printf("Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d no edificio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, moneyCount, pcurrent->estudios[index].id, pcurrent->id);
-			}
-			else if (strcmp(type, "file") == 0) {
-				FILE* fp = fopen("taxa_ocupacao.txt", "w");
-				if (fp == NULL) {
-					printf("Erro ao abrir ficheiro");
-				}
-				fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, houveram %d estadias no estudio %d no edificio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, moneyCount, pcurrent->estudios[index].id, pcurrent->id);
-				fclose(fp);
-			}
-			else {
-				printf("Opcao invalida");
-			}
+			estudioCount++;
 		}
-		moneyCount = 0;
 		pprev = pcurrent;
 		pcurrent = pcurrent->next;
+	} 
+	if (strcmp("decrescente", order) == 0) {
+		selectionSortDecrescente(taxa, estudioCount,"money");
+	}
+	else {
+		selectionSortCrescente(taxa, estudioCount, "money");
+	}
+	for (int i = 0; i < estudioCount; i++) {
+		if (strcmp(type, "print") == 0) {
+			printf("Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d no edificio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].moneyCount, taxa[i].estudio, taxa[i].edificio);
+		}
+		else if (strcmp(type, "file") == 0) {
+			FILE* fp = fopen("taxa_ocupacao.txt", "w");
+			if (fp == NULL) {
+				printf("Erro ao abrir ficheiro");
+			}
+			fprintf(fp, "Durante %d/%d/%d e %d/%d/%d, foram feitos %d euros no estudio %d no edificio %d\n", data_inicio_dia, data_inicio_mes, data_inicio_ano, data_fim_dia, data_fim_mes, data_fim_ano, taxa[i].moneyCount, taxa[i].estudio, taxa[i].edificio);
+			fclose(fp);
+		}
+		else {
+			printf("Opcao invalida");
+		}
 	}
 
 }
